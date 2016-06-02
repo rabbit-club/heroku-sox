13a1ee908d10ef5694ac2d2a6acd5d1e067290a4
村松 和也 <KazuyaMuramatsu@muramatsu-no-MacBook-Air.local>
2016-06-02 23:27:34 +0900

    modify sox command

--- web/index.php
+++ web/index.php
@@ -6,4 +6,4 @@ $file_name = $_FILE['audio_file']['name'];
 $file_name = substr($file_name, 0, strcspn($file_name, '.'));
 $tmp_path = $_FILE['audo_file']['tmp_name'];
 
-exec("sox {$tmp_path} ./{$file_name}.mp3");
+exec("../vendor/sox/bin/sox {$tmp_path} ./{$file_name}.mp3");
