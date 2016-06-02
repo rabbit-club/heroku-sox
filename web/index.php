fa73ebe3b6a834139d9a62798ba0fddb56a3a376
村松 和也 <KazuyaMuramatsu@muramatsu-no-MacBook-Air.local>
2016-06-02 23:16:59 +0900

    exec sox

--- web/index.php
+++ web/index.php
@@ -2,4 +2,8 @@
 
 require('../vendor/autoload.php');
 
-echo "Hello, World\n";
+$file_name = $_FILE['audio_file']['name'];
+$file_name = substr($file_name, 0, strcspn($file_name, '.'));
+$tmp_path = $_FILE['audo_file']['tmp_name'];
+
+exec("sox {$tmp_path} ./{$file_name}.mp3");
