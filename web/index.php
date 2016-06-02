6086aa07cc089e44068ae574ffe8931c0034c825
村松 和也 <KazuyaMuramatsu@muramatsu-no-MacBook-Air.local>
2016-06-03 00:37:44 +0900

    modify

--- web/index.php
+++ web/index.php
@@ -5,10 +5,10 @@ $env = '/app/vendor/sox/lib';
 putenv("LD_LIBRARY_PATH={$env}");
 
 echo 'hoge'."\n";
-$file_name = $_FILE['audio_file']['name'];
+$file_name = $_FILES['audio_file']['name'];
 echo $file_name."\n";
 $file_name = substr($file_name, 0, strcspn($file_name, '.'));
-$tmp_path = $_FILE['audio_file']['tmp_name'];
+$tmp_path = $_FILES['audio_file']['tmp_name'];
 
 echo $tmp_path."\n";
 exec("sox {$tmp_path} ./{$file_name}.mp3");
