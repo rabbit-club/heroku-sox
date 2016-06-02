02ee551d79950e2174ce1f9bacbfbf39ec61ecc5
村松 和也 <KazuyaMuramatsu@muramatsu-no-MacBook-Air.local>
2016-06-03 00:59:17 +0900

    modify

--- web/index.php
+++ web/index.php
@@ -7,9 +7,19 @@ putenv("LD_LIBRARY_PATH={$env}");
 $file_name = $_FILES['audio_file']['name'];
 $file_name = substr($file_name, 0, strcspn($file_name, '.'));
 $tmp_path = $_FILES['audio_file']['tmp_name'];
+$out_path = "/tmp/{$file_name}.mp3";
 
-echo $file_name."\n";
-echo $tmp_path."\n";
-$cmd = "sox {$tmp_path}.wav /tmp/{$file_name}.mp3";
-echo $cmd."\n";
+$cmd = "sox {$tmp_path}.wav {$out_path}";
 exec($cmd);
+
+$content_length = filesize($out_path);
+$read_data = file_get_contents($out_path);
+
+/* Output HTTP Header */
+header('Content-Disposition: inline; filename="'.basename($out_path).'"');
+header('Content-Length: '.$content_length);
+header('Content-Type: application/octet-stream');
+
+/* Output File Data */
+echo $read_data;
+unlink($out_path);
