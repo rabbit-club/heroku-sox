df48f6a02bbabb9af90bea6573cdbf711b218938
村松 和也 <KazuyaMuramatsu@muramatsu-no-MacBook-Air.local>
2016-06-02 23:45:09 +0900

    add path and env

--- web/index.php
+++ web/index.php
@@ -1,6 +1,11 @@
 <?php
 
 require('../vendor/autoload.php');
+$path = '../vendor/sox/bin';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+$env = '../vendor/sox/lib';
+$prev_env = getenv('LD_LIBRARY_PATH');
+putenv("LD_LIBRARY_PATH={$prev_env}:{$env}");
 
 $file_name = $_FILE['audio_file']['name'];
 $file_name = substr($file_name, 0, strcspn($file_name, '.'));
