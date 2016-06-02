b70a59d784e344e9074a6ef9fddabcafee34ef6b
村松 和也 <KazuyaMuramatsu@muramatsu-no-MacBook-Air.local>
2016-06-03 00:28:28 +0900

    modify

--- web/index.php
+++ web/index.php
@@ -1,15 +1,11 @@
 <?php
 
 require('../vendor/autoload.php');
-$path = '/app/vendor/sox/bin';
-set_include_path(get_include_path() . PATH_SEPARATOR . $path);
-echo get_include_path()."\n";
-$env = '/app/vendor/sox/lib';
-putenv("LD_LIBRARY_PATH={$env}");
-echo getenv('LD_LIBRARY_PATH')."\n";
 
 $file_name = $_FILE['audio_file']['name'];
 $file_name = substr($file_name, 0, strcspn($file_name, '.'));
 $tmp_path = $_FILE['audo_file']['tmp_name'];
 
+echo $tmp_path."\n";
+echo $file_name."\n";
 exec("sox {$tmp_path} ./{$file_name}.mp3");
