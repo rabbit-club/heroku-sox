7bc878ef06014741e283de21dc77dbb3df1ec5ee
村松 和也 <KazuyaMuramatsu@muramatsu-no-MacBook-Air.local>
2016-06-03 00:46:57 +0900

    modify

--- web/index.php
+++ web/index.php
@@ -4,11 +4,10 @@ require('../vendor/autoload.php');
 $env = '/app/vendor/sox/lib';
 putenv("LD_LIBRARY_PATH={$env}");
 
-echo 'hoge'."\n";
 $file_name = $_FILES['audio_file']['name'];
-echo $file_name."\n";
 $file_name = substr($file_name, 0, strcspn($file_name, '.'));
 $tmp_path = $_FILES['audio_file']['tmp_name'];
 
+echo $file_name."\n";
 echo $tmp_path."\n";
 exec("sox {$tmp_path}.wav /tmp/{$file_name}.mp3");
